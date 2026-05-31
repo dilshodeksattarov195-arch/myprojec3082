f0c24e62cad551bba6c6ea81e80b6b8fc6d8ae74
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-31 19:45:24 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class uploaderCerifyWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = uploaderCerifyWorker()
+    print("Worker engine initialized.")
